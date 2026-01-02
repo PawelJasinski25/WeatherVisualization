a35bc1d74f8928a75dcc32bcba35be8580384fef
PawelJasinski25 <paweljasinski257@gmail.com>
2026-01-02 18:18:11 +0100

    initial commit

---
+++ backend/src/main/java/jasinski/pawel/weather_visualization/WeatherVisualizationApplication.java
@@ -0,0 +1,13 @@
+package jasinski.pawel.weather_visualization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherVisualizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherVisualizationApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/jasinski/pawel/weather_visualization/WeatherVisualizationApplicationTests.java
@@ -0,0 +1,13 @@
+package jasinski.pawel.weather_visualization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherVisualizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
