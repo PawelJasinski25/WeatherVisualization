bb1e85cc32701a49e594882edfbbe2824f9b80b7
PawelJasinski25 <paweljasinski257@gmail.com>
2026-06-01 19:47:28 +0200

    optimize water detection with json streaming

--- backend/src/main/java/jasinski/pawel/weather_visualization/service/WaterDetectionService.java
+++ backend/src/main/java/jasinski/pawel/weather_visualization/service/WaterDetectionService.java
@@ -1,5 +1,7 @@
 package jasinski.pawel.weather_visualization.service;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.PostConstruct;
@@ -20,33 +22,44 @@ public class WaterDetectionService {
 
     @PostConstruct
     public void init() {
+
         try (InputStream is = getClass().getResourceAsStream("/water.geojson")) {
             if (is == null) {
-                System.out.println("Brak pliku water.geojson w resources");
+                System.err.println("Błąd: Brak pliku water.geojson w resources");
                 return;
             }
 
             ObjectMapper mapper = new ObjectMapper();
-            JsonNode root = mapper.readTree(is);
             List<Polygon> polygons = new ArrayList<>();
 
-            JsonNode features = root.path("features");
-            for (JsonNode feature : features) {
-                JsonNode geometry = feature.path("geometry");
-                String type = geometry.path("type").asText();
-                JsonNode coords = geometry.path("coordinates");
+            try (JsonParser parser = mapper.getFactory().createParser(is)) {
 
-                if ("Polygon".equals(type)) {
-                    polygons.add(parsePolygon(coords));
-                } else if ("MultiPolygon".equals(type)) {
-                    for (JsonNode polyNode : coords) {
-                        polygons.add(parsePolygon(polyNode));
+                while (parser.nextToken() != null) {
+                    if ("features".equals(parser.getCurrentName())) {
+                        parser.nextToken();
+
+
+                        while (parser.nextToken() == JsonToken.START_OBJECT) {
+                            JsonNode feature = mapper.readTree(parser);
+                            JsonNode geometry = feature.path("geometry");
+                            String type = geometry.path("type").asText();
+                            JsonNode coords = geometry.path("coordinates");
+
+                            if ("Polygon".equals(type)) {
+                                polygons.add(parsePolygon(coords));
+                            } else if ("MultiPolygon".equals(type)) {
+                                for (JsonNode polyNode : coords) {
+                                    polygons.add(parsePolygon(polyNode));
+                                }
+                            }
+                        }
                     }
                 }
             }
 
             MultiPolygon multiPolygon = factory.createMultiPolygon(polygons.toArray(new Polygon[0]));
             this.waterGeometry = PreparedGeometryFactory.prepare(multiPolygon);
+
 
         } catch (Exception e) {
             System.err.println("Błąd ładowania geojson: " + e.getMessage());
